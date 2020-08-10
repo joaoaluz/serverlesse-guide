3e601f7cce0322a8b58a33e3b92c5e87548a7b2f
joaoaluz <joao_aluz@hotmail.com>
2020-08-10 11:03:23 -0300

    add list API

--- create.js
+++ create.js
@@ -19,6 +19,7 @@ export const main = handler(async (event, context) => {
       noteId: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
+      teste: data.teste,
       createdAt: Date.now()
     }
   };
